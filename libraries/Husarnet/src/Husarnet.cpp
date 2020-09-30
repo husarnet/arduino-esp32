6239a486f95c4e26f1a03788d79e8526dbac1f40
Paweł pidpawel Kozubal <pawel.kozubal@husarnet.com>
2020-09-30 16:50:07 +0200

    Added methods for getting lists of peers and a current hostname

--- libraries/Husarnet/src/Husarnet.cpp
+++ libraries/Husarnet/src/Husarnet.cpp
@@ -1,13 +1,21 @@
 #include "Husarnet.h"
 #include <cassert>
 
+
 _Husarnet Husarnet;
 
 extern "C" {
     void husarnet_start();
     void husarnet_join(const char* joinCode, const char* hostname);
     void husarnet_retrieve_license(const char* hostname);
+    const char* husarnet_get_hostname();
 }
+
+struct InternalIP6Address {
+    std::array<uint8_t, 16> data;
+};
+
+extern std::vector<std::pair<InternalIP6Address, std::string>> husarnet_hosts;
 
 static bool alreadyStarted = false;
 
@@ -25,4 +33,19 @@ void _Husarnet::start() {
 void _Husarnet::join(const char* joinCode, const char* hostname) {
     assert(!alreadyStarted);
     husarnet_join(joinCode, hostname);
+}
+
+typedef std::pair<IPv6Address, String> hostPair;
+std::vector<hostPair> _Husarnet::listPeers() {
+    std::vector<hostPair> peers;
+
+    for(auto const& host: husarnet_hosts) {
+        peers.push_back(hostPair(IPv6Address(host.first.data.data()), String(host.second.c_str())));
+    }
+
+    return peers;
+}
+
+String _Husarnet::getHostname() {
+    return String(husarnet_get_hostname());
 }
--- libraries/Husarnet/src/Husarnet.h
+++ libraries/Husarnet/src/Husarnet.h
@@ -3,17 +3,28 @@
 
 #include "Arduino.h"
 #include "HusarnetServer.h"
-//#include <freertos/task.h>
+#include "IPv6Address.h"
+#include "WString.h"
+#include <vector>
+#include <utility>
+#include <string>
 
-struct _Husarnet {
+struct _Husarnet
+{
     // Sets up Husarnet to use self-hosted base server
-    void selfHostedSetup(const char* hostname);
+    void selfHostedSetup(const char *hostname);
 
     // Starts the Husarnet
     void start();
 
     // Provides join code. Use before Husarnet.start().
-    void join(const char* joinCode, const char* hostname="");
+    void join(const char *joinCode, const char *hostname = "");
+
+    // Get list of peers' hostnames and addresses
+    std::vector<std::pair<IPv6Address, String>> listPeers();
+
+    // Get hostname you're currently known at
+    String getHostname();
 };
 
 extern _Husarnet Husarnet;
